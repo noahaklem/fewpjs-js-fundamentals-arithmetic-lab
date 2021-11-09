cfac3fa1d30d1896da597b97160095cd4054f865
Noah A Klem <noahaklem@gmail.com>
2021-11-08 17:04:58 -0700

    commit tree 8a6e64481be19f99fd94248094b771a21ad4cd8a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
 
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 28;
+const num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 20, 3);
 // Write your code here
